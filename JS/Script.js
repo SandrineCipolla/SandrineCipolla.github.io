f36a9b2395492903367fdf9ef741330d9d76282e
Sandrine Cipolla <sandrine.cipolla@gmail.com>
2023-03-19 21:25:46 +0100

    filtres sur le portfolio

--- JS/Script.js
+++ JS/Script.js
@@ -6,6 +6,7 @@
 // let grids = document.querySelectorAll(".grid");
 // console.log(grids);
 
+/*Menu Mobile*/
 function menuMobile() {
   let btn = document.querySelector(".burger");
   let header = document.querySelector(".header");
@@ -21,3 +22,63 @@ function menuMobile() {
   });
 }
 menuMobile();
+
+/*Portfolio*/
+function tabsFilters() {
+  let tabs = document.querySelectorAll(".portfolio-filters a");
+  let projects = document.querySelectorAll(".portfolio .card");
+
+  let resetActiveLinks = () => {
+    tabs.forEach((element) => {
+      element.classList.remove("active");
+    });
+  };
+
+  let showProjects = (element) => {
+    console.log(element);
+    projects.forEach((project) => {
+      let filter = project.getAttribute("data-category");
+
+      if (element === "all") {
+        project.parentNode.classList.remove("hide");
+        return;
+      }
+      console.log("tutu");
+
+      //   if (filter !== element) {
+      //     project.parentNode.classList.add("hide");
+      //   } else {
+      //     project.parentNode.classList.remove("hide");
+      //   }
+
+      //conditions en opérateur ternaire:
+      filter !== element
+        ? project.parentNode.classList.add("hide")
+        : project.parentNode.classList.remove("hide");
+    });
+  };
+
+  tabs.forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      let filter = element.getAttribute("data-filter");
+      showProjects(filter);
+      resetActiveLinks();
+      element.classList.add("active");
+    });
+  });
+}
+tabsFilters();
+
+function showProjectDetails() {
+  let links = document.querySelectorAll(".card__link");
+  let modals = document.querySelectorAll(".modal");
+
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.querySelector(`[id=${link.dataset.id}]`).classList.add("show");
+    });
+  });
+}
+showProjectDetails();
